b9e51c27020df3afc4cd968a1f4704da7f5d0636
ElyasafCohen100 <elyasaf93@gmail.com>
2021-10-10 02:55:03 +0300

    initial code + welcome functions

---
+++ Targil0/Program7227.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7227();
+            Welcome3024();
+            Console.ReadKey();
+        }
+        static partial void Welcome3024();
+        private static void Welcome7227()
+        {
+            Console.Write("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
