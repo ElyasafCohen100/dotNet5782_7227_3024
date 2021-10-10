2691949418eb3221ba032ddc563028d889a54b4e
yohanan400 <yohananyakir@gmail.com>
2021-10-10 12:34:04 +0300

    initial code the colab function

---
+++ Targil0/Program2034.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome3024()
+        {
+            Console.WriteLine("Hello this is the other one");
+        }
+
+    }
+}
